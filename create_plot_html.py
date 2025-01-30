bf7d93e303dd0a40f2a443f8dbfe1e7a88760c30
pritam-cccr <pritam.mahapatra@tropmet.res.in>
2025-01-30 17:20:32 +0530

    Update create_plot_html.py

--- create_plot_html.py
+++ create_plot_html.py
@@ -1,4 +1,11 @@
-# Praveen V
+# ==============================================================================
+# Copyright (C) 2025 Centre for Climate Change Research (CCCR), IITM
+#
+# This script is part of the CCCR IITM_ESM diagnostics system.
+#
+# Author: [Praveen V; Pritam Das Mahapatra]
+# Date: January 2025
+# ==============================================================================
 import sys
 import os
 
